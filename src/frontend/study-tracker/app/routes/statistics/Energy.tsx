af11fedd6280caaba117b12679e867eab78a586e
Miguel Rocha <miguelasrocha1@gmailcom>
2024-10-15 01:00:34 +0100

    feat: improves visuals of daily energy statistics page

--- src/frontend/study-tracker/app/routes/statistics/Energy.tsx
+++ src/frontend/study-tracker/app/routes/statistics/Energy.tsx
@@ -5,6 +5,77 @@ import { useSetGlobalError } from "~/components/error/GlobalErrorContainer"
 import { DailyEnergyStatus, service } from "~/service/service"
 import { utils } from "~/utils";
 
+function EnergyStatus({status} : {status: DailyEnergyStatus}) {
+    return (
+        <div>
+            <p>(O)</p>
+            <span>Today, {status.date.getDate()} {status.date.toLocaleString('default', { month: 'long' })}</span>
+        </div>
+    )
+}
+
+function EnergyStatusHistory({energyHistory} : {energyHistory: DailyEnergyStatus[]}) {
+    return (
+        <>
+            <div className={styles.historyFirstContainer}>
+                <span className={styles.historyTitle}>History</span>
+                <span className={styles.seeFullHistoryText}>(O) See full history</span>
+            </div>
+
+            <div className={styles.historyStatusAndDate}>
+                {energyHistory.map((value: DailyEnergyStatus) => 
+                    <EnergyStatus status={value} />
+                )}
+            </div>
+        </>
+    )
+}
+
+function levelToStr(level: number): string {
+    if (level >= 9) return "Very Well"
+    if (level >= 7) return "Well"
+    if (level >= 5) return "Good"
+    return "Alive"
+}
+
+function levelToColor(level: number): string {
+    if (level >= 9) return styles.verWellColor
+    if (level >= 7) return styles.wellColor
+    if (level >= 5) return styles.goodColor
+    return styles.badColor
+}
+
+function TodayDate() {
+    const today = new Date()
+    return (
+        <div className={styles.todayDate}>
+            Today, {today.getDate()} {today.toLocaleString('default', { month: 'long' })}
+        </div>
+    )
+}
+
+function TodayEnergyStatus({status} : {status: DailyEnergyStatus | undefined}) {
+    return (
+        <>
+            <TodayDate />
+            <br/><br/>
+            {status ?
+                <span className={classNames(levelToColor(status.level), styles.todayEnergyStatusTitle)}>
+                    (SYMBOL) I feel {levelToStr(status.level)}
+                </span>
+                :
+                <p>Nothing</p>
+            }
+
+            <br/><br/>
+
+            <span>
+                TAGS
+            </span>
+        </>
+    )
+}
+
 function useEnergyStats() {
     const setError = useSetGlobalError()
     const [energyHistory, setEnergyHistory] = useState<DailyEnergyStatus[] | undefined>(undefined)
@@ -25,40 +96,6 @@ function useEnergyStats() {
     }
 }
 
-function levelToStr(level: number): string {
-    if (level >= 9) return "Very Well"
-    if (level >= 7) return "Well"
-    if (level >= 5) return "Good"
-    return "Alive"
-}
-
-function levelToColor(level: number): string {
-    if (level >= 9) return styles.verWellColor
-    if (level >= 7) return styles.wellColor
-    if (level >= 5) return styles.goodColor
-    return styles.badColor
-}
-
-function TodayEnergyStatus({status} : {status: DailyEnergyStatus | undefined}) {
-    return (
-        <>
-            {status ?
-                <span className={classNames(levelToColor(status.level), styles.todayEnergyStatusTitle)}>
-                    (SYMBOL) I feel {levelToStr(status.level)}
-                </span>
-                :
-                <p>Nothing</p>
-            }
-
-            <br/><br/>
-
-            <div>
-                TAGS
-            </div>
-        </>
-    )
-}
-
 export function EnergyStats() {
     const {
         energyHistory, 
@@ -68,9 +105,13 @@ export function EnergyStats() {
         <>
             <div className={styles.statsContainer}>
                 <div className={styles.statTitle}>
-                    Energy
+                    (O) Energy
                 </div>
                 <TodayEnergyStatus status={getTodayEnergyStatus()} />
+                
+                <br/><br/>
+
+                <EnergyStatusHistory energyHistory={energyHistory ? energyHistory.slice(0, 6) : []} />
             </div>
         </>
     )
