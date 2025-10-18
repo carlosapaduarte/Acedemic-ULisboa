e36d97c422a78eac5c2a304127c6c682003a754e
alexandraat <alexandrateixeira002@gmail.com>
2025-10-18 22:13:40 +0100

    config edit

--- src/frontend/acedemic-challenge/app/service/config.ts
+++ src/frontend/acedemic-challenge/app/service/config.ts
@@ -1,2 +1,2 @@
-//export const BASE_API_URL = "/api/";
-export const BASE_API_URL = import.meta.env.VITE_API_URL ?? "/api/";
+export const BASE_API_URL = "/api/";
+//export const BASE_API_URL = import.meta.env.VITE_API_URL ?? "/api/";
